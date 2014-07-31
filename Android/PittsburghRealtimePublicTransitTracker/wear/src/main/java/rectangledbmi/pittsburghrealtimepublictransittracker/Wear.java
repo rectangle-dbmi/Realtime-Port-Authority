fdbc18c90412ca64cb487d53e5b934f25028079e
epicstar <myramenisgone@gmail.com>
2014-07-31 13:36:43 -0400

    Remade the program... Apparently moved to ANT at some point... Have the Bus and SAXHandler and Android Wear too.

---
+++ Android/PittsburghRealtimePublicTransitTracker/wear/src/main/java/rectangledbmi/pittsburghrealtimepublictransittracker/Wear.java
@@ -0,0 +1,24 @@
+package rectangledbmi.pittsburghrealtimepublictransittracker;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.wearable.view.WatchViewStub;
+import android.widget.TextView;
+
+public class Wear extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wear);
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+            }
+        });
+    }
+}
---
+++ Android/PittsburghRealtimePublicTransitTracker/world/src/main/java/com/example/ourmodule/world/Bus.java
@@ -0,0 +1,89 @@
+package com.example.ourmodule.world;
+
+public class Bus {
+
+    private String vid;
+    private String lat;
+    private String lon;
+    private String msg;
+    private String tmstmp;
+    private String hdg;
+    private String pid;
+    private String rt;
+    private String des;
+    private String pdist;
+    private String spd;
+    private String tablockid;
+    private String tatripid;
+
+    public float getLat() {
+        return Float.parseFloat(lat);
+    }
+
+    public float getLon() {
+        return Float.parseFloat(lon);
+    }
+
+    public int getVid() {
+        return Integer.parseInt(vid);
+    }
+
+    public void setVid(String vid) {
+        this.vid = vid;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public void setLon(String lon) {
+        this.lon = lon;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public void setTmStmp(String tmStmp) {
+        this.tmstmp = tmStmp;
+    }
+
+    public void setHdg(String hdg) {
+        this.hdg = hdg;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public void setRt(String rt) {
+        this.rt = rt;
+    }
+
+    public void setDes(String des) {
+        this.des = des;
+    }
+
+    public void setPdist(String pdist) {
+        this.pdist = pdist;
+    }
+
+    public void setSpd(String spd) {
+        this.spd = spd;
+    }
+
+    public void setTablockid(String tablockid) {
+        this.tablockid = tablockid;
+    }
+
+    public void setTatripid(String tatripid) {
+        this.tatripid = tatripid;
+    }
+
+    @Override
+    public String toString() {
+        return vid + "\t" + lat + "\t" + lon + "\t" + tmstmp + "\t" + hdg
+                + "\t" + pid + "\t" + rt + "\t" + des + "\t" + pdist + "\t"
+                + spd + "\t" + tablockid + "\t" + tatripid;
+    }
+}
