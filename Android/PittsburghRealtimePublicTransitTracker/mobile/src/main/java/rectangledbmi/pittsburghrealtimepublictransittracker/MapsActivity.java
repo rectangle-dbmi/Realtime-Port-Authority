2c7a99a0eac9e166739dc40c2e3c07920b8fa534
Ritwik Gupta <ritwikg2004@live.com>
2014-08-04 23:23:58 -0400

    Added line to MapsActivity

--- Android/PittsburghRealtimePublicTransitTracker/mobile/src/main/java/rectangledbmi/pittsburghrealtimepublictransittracker/MapsActivity.java
+++ Android/PittsburghRealtimePublicTransitTracker/mobile/src/main/java/rectangledbmi/pittsburghrealtimepublictransittracker/MapsActivity.java
@@ -29,6 +29,7 @@ public class MapsActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
+        new RequestTask(mMap).execute();
     }
 
     /**
