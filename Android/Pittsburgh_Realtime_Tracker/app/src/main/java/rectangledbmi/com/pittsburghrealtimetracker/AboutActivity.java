dba9d6b0c9327e300fc7a860f222aba674e1d56f
epicstar <myramenisgone@gmail.com>
2014-11-09 18:55:07 -0500

    Changed more of the About to use a scrollview and a disclaimer

--- Android/Pittsburgh_Realtime_Tracker/app/src/main/java/rectangledbmi/com/pittsburghrealtimetracker/AboutActivity.java
+++ Android/Pittsburgh_Realtime_Tracker/app/src/main/java/rectangledbmi/com/pittsburghrealtimetracker/AboutActivity.java
@@ -50,9 +50,9 @@ public class AboutActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
         return super.onOptionsItemSelected(item);
     }
 
