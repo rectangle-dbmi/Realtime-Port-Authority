a13f37084dc737103b8d780dd384b9dbdb452c50
Ritwik Gupta <ritwikg2004@live.com>
2014-12-17 20:21:42 -0500

    Added Predictions parser

---
+++ Android/Pittsburgh_Realtime_Tracker/app/src/main/java/rectangledbmi/com/pittsburghrealtimetracker/handlers/PredictionsXMLPullParser.java
@@ -0,0 +1,120 @@
+package rectangledbmi.com.pittsburghrealtimetracker.handlers;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+
+import rectangledbmi.com.pittsburghrealtimetracker.world.Prediction;
+
+/**
+ * Created by rgupta on 12/17/14.
+ */
+public class PredictionsXMLPullParser {
+
+    private List<Prediction> predictionList;
+    private URL url;
+    XmlPullParser parser;
+
+    public PredictionsXMLPullParser(URL url) throws XmlPullParserException {
+
+        this.url = url;
+        XmlPullParserFactory pullParserFactory = XmlPullParserFactory.newInstance();
+        parser = pullParserFactory.newPullParser();
+    }
+
+    /**
+     * Creates the bus list by starting the XMLPullParser
+     *
+     * @return the list of predictions
+     * @throws java.io.IOException
+     * @throws XmlPullParserException
+     */
+    public List<Prediction> createPredictionList() throws IOException, XmlPullParserException {
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setConnectTimeout(5000);
+        parser.setInput(conn.getInputStream(), null);
+        parseXML();
+        return getPredictionList();
+    }
+
+    /**
+     * @return the list of buses
+     */
+    public List<Prediction> getPredictionList() {
+        return predictionList;
+    }
+
+    /**
+     * XMLPullParser implementation of getting predictions from the Port Authority API
+     *
+     * @throws XmlPullParserException
+     * @throws IOException
+     */
+    private void parseXML() throws XmlPullParserException, IOException {
+
+        int eventType = parser.getEventType();
+        Prediction prediction = new Prediction();
+        while (eventType != XmlPullParser.END_DOCUMENT) {
+            String name = parser.getName();
+
+            try {
+                switch (eventType) {
+
+                    case (XmlPullParser.START_TAG): {
+                        if ("stpid".equals(name)) { //new vehicle seen and make sure nothing else is there
+                            prediction = new Prediction();
+                            prediction.setStpid(parser.nextText());
+                        } else {
+                            //below is to add a new vehicle
+                            if ("vid".equals(name)) {
+                                prediction.setVid(parser.nextText());
+                            } else if ("tmstmp".equals(name)) {
+                                prediction.setTmpstmp(parser.nextText());
+                            } else if ("rt".equals(name)) {
+                                prediction.setRt(parser.nextText());
+                            } else if ("des".equals(name)) {
+                                prediction.setDes(parser.nextText());
+                            } else if ("dly".equals(name)) {
+                                prediction.setDly(parser.nextText());
+                            } else if ("tablockid".equals(name)) {
+                                prediction.setTablockid(parser.nextText());
+                            } else if ("tatripid".equals(name)) {
+                                prediction.setTatripid(parser.nextText());
+                            } else if ("typ".equals(name)) {
+                                prediction.setTyp(parser.nextText());
+                            } else if ("stpnm".equals(name)) {
+                                prediction.setStpnm(parser.nextText());
+                            } else if ("dstp".equals(name)) {
+                                prediction.setDstp(parser.nextText());
+                            } else if ("rtdir".equals(name)) {
+                                prediction.setRtdir(parser.nextText());
+                            } else if ("prdtm".equals(name)) {
+                                prediction.setPrdtm(parser.nextText());
+                            } else if ("prtctdn".equals(name)) {
+                                prediction.setPrdctdn(parser.nextText());
+                            }
+                        }
+                        break;
+                    }
+                    case (XmlPullParser.END_TAG): { //adds to new vehicle
+                        if ("stpid".equals(name)) {
+                            predictionList.add(prediction);
+                        }
+                        break;
+                    }
+
+                }
+            } catch (NullPointerException e) {
+                System.err.println("Prediction get error.");
+            }
+            eventType = parser.next();
+        }
+    }
+
+}
+
