ed42a10cad80fa14f8ab30d04cc93af6a1be763d
Jeremy Jao <myramenisgone@gmail.com>
2015-07-07 22:50:27 -0400

    attempting to remove gradle properties again and edited patapi

--- Android/Pittsburgh_Realtime_Tracker/app/src/main/java/rectangledbmi/com/pittsburghrealtimetracker/retrofit/PATAPI.java
+++ Android/Pittsburgh_Realtime_Tracker/app/src/main/java/rectangledbmi/com/pittsburghrealtimetracker/retrofit/PATAPI.java
@@ -1,12 +1,13 @@
 package rectangledbmi.com.pittsburghrealtimetracker.retrofit;
 
+import rectangledbmi.com.pittsburghrealtimetracker.BuildConfig;
 import rectangledbmi.com.pittsburghrealtimetracker.hidden.HiddenConstants;
 import rectangledbmi.com.pittsburghrealtimetracker.world.jsonpojo.PatternResponse;
 import rectangledbmi.com.pittsburghrealtimetracker.world.jsonpojo.PredictionResponse;
 import rectangledbmi.com.pittsburghrealtimetracker.world.jsonpojo.VehicleResponse;
-import retrofit.Callback;
 import retrofit.http.GET;
 import retrofit.http.Path;
+import rx.Observable;
 
 /**
  * This is the general api that sets the retrofit api. You must have tte {@link HiddenConstants} class
@@ -16,41 +17,48 @@ import retrofit.http.Path;
  * @since 46
  */
 public interface PATAPI {
-
+    String key = BuildConfig.PAT_API_KEY;
     /**
      * generates a response for patters
      * @param rt - the route
-     * @param responseCallback - the callback
+     * @param api_key - the api key
      */
-    @GET("/getpatterns?format=json&&rt={route}&key=" + HiddenConstants.PAT_API_KEY)
-    void getPatterns(@Path("route") String rt, Callback<PatternResponse> responseCallback);
+    @GET("/getpatterns?format=json&&rt={route}&key={api_key}" )
+    Observable<PatternResponse> getPatterns(@Path("route") String rt, @Path("api_key") String api_key);
 
     /**
      * Generates a response to get vehicles
      * @param routes - the routes
-     * @param responseCallback - the callback
+     * @param api_key - the api key
      */
-    @GET("/getvehicles?format=json&&rt={routes}&key=" + HiddenConstants.PAT_API_KEY)
-    void getVehicles(@Path("routes") String routes, Callback<VehicleResponse> responseCallback);
+    @GET("/getvehicles?format=json&&rt={routes}&key={api_key}")
+    Observable<VehicleResponse> getVehicles(@Path("routes") String routes, @Path("api_key") String api_key);
 
     /**
-     * Generates a response to get predictions when clicking on a bus
+     * Generates a response to get the predictions using the stop id
+     * @param stpid - the stop id
+     * @param rts - the routes
+     * @param api_key - the api key
+     */
+    @GET("/getpredictions?format=json&stpid={stpid}&key={api_key}")
+    Observable<PredictionResponse> getStopPredictions(@Path("stpid") int stpid, @Path("api_key") String api_key);
+
+    /**
+     * Generates a response to get the predictions using the stop id
+     * @param stpid - the stop id
+     * @param rts - the routes
+     * @param api_key - the api key
+     */
+    @GET("/getpredictions?format=json&stpid={stpid}&rt={rts}&key={api_key}")
+    Observable<PredictionResponse> getStopPredictions(@Path("stpid") int stpid, @Path("rts") String rts, @Path("api_key") String api_key);
+
+    /**
+     * Generates a response to get the predictions using the bus id
      * @param vid - the bus id
-     * @param responseCallback - the callback
+     * @param api_key - the api key
      */
-    @GET("/getpredictions?format=json&vid={vid}&key=" + HiddenConstants.PAT_API_KEY)
-    void getBusPredictions(@Path("vid") int vid, Callback<PredictionResponse> responseCallback);
-
-    /**
-     * Generates a response to get the predictions
-     * @param stpid - the bus id
-     * @param responseCallback
-     */
-    @GET("/getpredictions?format=json&stpid={stpid}&key=" + HiddenConstants.PAT_API_KEY)
-    void getStopPredictions(@Path("stpid") int stpid, Callback<PredictionResponse> responseCallback);
-
-    @GET("/getpredictions?format=json&stpid={stpid}&rt={rts}&key=" + HiddenConstants.PAT_API_KEY)
-    void getStopPredictions(@Path("stpid") int stpid, @Path("rts") String rts, Callback<PredictionResponse> responseCallback);
+    @GET("/getpredictions?format=json&vid={vid}&key={api_key}")
+    Observable<PredictionResponse> getBusPrediction(@Path("vid") int vid, @Path("api_key") String api_key);
 
 
 }
