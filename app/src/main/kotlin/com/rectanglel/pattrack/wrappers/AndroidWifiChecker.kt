57fedd65f301b56ce7871b3c84e0960b24a952cf
Jeremy Jao <myramenisgone@gmail.com>
2017-08-13 21:30:41 -0400

    #319: interface wrapper around Android Wifi state
    Not quite finished. We should edit the unit tests in PatternDataManager, move it to PatternDataManagerTestKt, then rename this to patternDataManagerTest.kt

---
+++ app/src/main/kotlin/com/rectanglel/pattrack/wrappers/AndroidWifiChecker.kt
@@ -0,0 +1,23 @@
+package com.rectanglel.pattrack.wrappers
+
+import android.content.Context
+import android.net.ConnectivityManager
+import com.rectanglel.patstatic.wrappers.WifiChecker
+
+/**
+ * <p>Android implementation of checking to see if wifi is on.
+ * <p>Created by epicstar on 8/13/17.
+ * @author Jeremy Jao
+ * @since 160080002
+ */
+class AndroidWifiChecker constructor(context: Context): WifiChecker {
+
+    private val connectivityManager : ConnectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    override fun isOnWifi(): Boolean {
+        val networkInfo = connectivityManager.activeNetworkInfo
+        return networkInfo.type == ConnectivityManager.TYPE_WIFI
+    }
+
+}
