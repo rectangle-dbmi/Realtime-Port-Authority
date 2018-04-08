7c31ea91c0c3d7f1ab882cad34d13288924886e7
Jeremy Jao <myramenisgone@gmail.com>
2018-04-08 14:59:23 -0400

    update build versions again

--- pat-static/src/main/kotlin/com/rectanglel/patstatic/patterns/PatternDataManager.kt
+++ pat-static/src/main/kotlin/com/rectanglel/patstatic/patterns/PatternDataManager.kt
@@ -1,18 +1,21 @@
 package com.rectanglel.patstatic.patterns
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import com.google.gson.reflect.TypeToken
-import com.rectanglel.patstatic.model.AbstractDataManager
+import com.google.gson.stream.JsonReader
+import com.google.gson.stream.JsonWriter
 import com.rectanglel.patstatic.model.RetrofitPatApi
 import com.rectanglel.patstatic.model.StaticData
 import com.rectanglel.patstatic.patterns.response.Ptr
 import com.rectanglel.patstatic.wrappers.WifiChecker
 import rx.Observable
 import rx.exceptions.Exceptions
-import java.io.File
-import java.io.IOException
+import java.io.*
+import java.util.concurrent.locks.ReentrantReadWriteLock
 
 private val dataType = object : TypeToken<List<Ptr>>() {}.type!!
-private val maxFileAge = 24 * 60 * 1000 * 1000 // 24 hours
+private const val maxFileAge = 24 * 60 * 1000 * 1000 // 24 hours
 
 /**
  * Create a data manager for pattern selections that will handle:
@@ -27,13 +30,12 @@ private val maxFileAge = 24 * 60 * 1000 * 1000 // 24 hours
  */
 class PatternDataManager(dataDirectory : File,
                                  private val patApiClient: RetrofitPatApi,
-                                 staticData: StaticData,
-                                 private val wifiChecker: WifiChecker)
-    : AbstractDataManager<List<Ptr>>(
-        dataDirectory,
-        staticData,
-        dataType,
-        "lineinfo") {
+                                 val staticData: StaticData,
+                                 private val wifiChecker: WifiChecker) {
+
+    private val cacheFolderName = "lineinfo"
+    private val serializedType = dataType
+
 
     fun getPatterns(rt: String) : Observable<List<Ptr>> {
         val polylineFile = getPatternsFile(rt)
@@ -85,5 +87,80 @@ class PatternDataManager(dataDirectory : File,
                 .onErrorResumeNext { getPatternsFromDisk(rt) }
     }
 
+    private val rwl: ReentrantReadWriteLock = ReentrantReadWriteLock()
+    protected val dataDirectory: File = File(dataDirectory, cacheFolderName)
+    private val readLock: ReentrantReadWriteLock.ReadLock
+        get() = rwl.readLock()
+
+    private val writeLock: ReentrantReadWriteLock.WriteLock
+        get() = rwl.writeLock()
+
+    protected val gson: Gson
+        get() = GsonBuilder().create()
+
+    init {
+        this.dataDirectory.mkdirs()
+    }
+
+    /**
+     * Save the object to disk as JSON into the file
+     * @param obj the object to save
+     * @param file the file to save to
+     * @throws IOException if the serialization fails
+     */
+    @Throws(IOException::class)
+    protected fun saveAsJson(obj: Any, file: File) {
+        writeLock.lock()
+        val fileWriter = FileWriter(file)
+        val writer = JsonWriter(fileWriter)
+        try {
+            val gson = gson
+            gson.toJson(obj, serializedType, writer)
+        } finally {
+            writer.flush()
+            fileWriter.close()
+            writer.close()
+            writeLock.unlock()
+        }
+    }
+
+    @Throws(IOException::class)
+    protected fun getFromDisk(file: File): List<Ptr> {
+        // file exists... get from disk
+        if (file.exists()) {
+            readLock.lock()
+            val fileReader = FileReader(file)
+            val jsonReader = JsonReader(fileReader)
+            try {
+                return gson.fromJson(jsonReader, serializedType)
+            } finally {
+                fileReader.close()
+                jsonReader.close()
+                readLock.unlock()
+            }
+        }
+        // if file doesn't exist, save bundled file from installation to disk
+        val fileName = String.format("%s/%s", cacheFolderName, file.name)
+        copyStreamToFile(staticData.getInputStreamForFileName(fileName), file)
+        return getFromDisk(file)
+    }
+
+    @Throws(IOException::class)
+    protected fun copyStreamToFile(reader: InputStreamReader, file: File) {
+        writeLock.lock()
+
+
+        try {
+            reader.use { input ->
+                val fileWriter = FileWriter(file)
+                fileWriter.use {
+                    input.copyTo(it)
+                }
+            }
+        } finally {
+            writeLock.unlock()
+        }
+
+    }
 
 }
