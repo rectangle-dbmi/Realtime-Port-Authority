b1d53c0349c0e4a8a75e9de80058ff6767ae615e
Jeremy Jao <myramenisgone@gmail.com>
2018-04-15 16:37:54 -0400

    clean code

--- pat-static/src/main/java/io/paour/github/natorder/NaturalOrderComparator.java
+++ pat-static/src/main/java/io/paour/github/natorder/NaturalOrderComparator.java
@@ -76,7 +76,7 @@ public class NaturalOrderComparator<T> implements Comparator<T> // epicstar: cha
         String b = o2.toString();
 
         int ia = 0, ib = 0;
-        int nza = 0, nzb = 0;
+        int nza, nzb;
         char ca, cb;
         int result;
 
@@ -159,26 +159,5 @@ public class NaturalOrderComparator<T> implements Comparator<T> // epicstar: cha
         {
             return s.charAt(i);
         }
-    }
-
-    public static void main(String[] args)
-    {
-        String[] strings = new String[] { "1-2", "1-02", "1-20", "10-20", "fred", "jane", "pic01",
-                "pic2", "pic02", "pic02a", "pic3", "pic4", "pic 4 else", "pic 5", "pic05", "pic 5",
-                "pic 5 something", "pic 6", "pic   7", "pic100", "pic100a", "pic120", "pic121",
-                "pic02000", "tom", "x2-g8", "x2-y7", "x2-y08", "x8-y8" };
-
-        List orig = Arrays.asList(strings);
-
-        System.out.println("Original: " + orig);
-
-        List scrambled = Arrays.asList(strings);
-        Collections.shuffle(scrambled);
-
-        System.out.println("Scrambled: " + scrambled);
-
-        Collections.sort(scrambled, new NaturalOrderComparator());
-
-        System.out.println("Sorted: " + scrambled);
     }
 }
