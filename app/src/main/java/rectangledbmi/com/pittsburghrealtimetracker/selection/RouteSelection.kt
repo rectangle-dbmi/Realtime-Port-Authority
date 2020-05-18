d1af226ccee13456ea32d55df63a8e339fcd4fff
mikeantonacci <michaelantonacci2@gmail.com>
2020-05-18 01:27:07 -0400

    RouteSelection and VehicleBitmap to kotlin

---
+++ app/src/main/java/rectangledbmi/com/pittsburghrealtimetracker/selection/RouteSelection.kt
@@ -0,0 +1,25 @@
+package rectangledbmi.com.pittsburghrealtimetracker.selection
+
+import timber.log.Timber
+
+/**
+ * An object that contains the selected route and/or all selected routes.
+ *
+ * Primarily used in the selection subject
+ *
+ * @author Jeremy Jao
+ */
+class RouteSelection private constructor(val toggledRoute: Route, val selectedRoutes: Set<String>) {
+
+    init {
+        Timber.d("getting both routes: %s, %s", toggledRoute.route, selectedRoutes.toString())
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun create(route: Route, selectedRoutes: Set<String>): RouteSelection {
+            return RouteSelection(route, selectedRoutes)
+        }
+    }
+}
---
+++ app/src/main/java/rectangledbmi/com/pittsburghrealtimetracker/vehicles/VehicleBitmap.kt
@@ -0,0 +1,11 @@
+package rectangledbmi.com.pittsburghrealtimetracker.vehicles
+
+import android.graphics.Bitmap
+
+import com.rectanglel.patstatic.vehicles.response.Vehicle
+
+/**
+ * This is a container that contains a [Vehicle] and a [Bitmap]
+ * @since 57
+ */
+class VehicleBitmap(val vehicle: Vehicle, val bitmap: Bitmap)
